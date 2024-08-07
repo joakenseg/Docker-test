1f0e30e17ba840af5dcea68cef8f5078488cda60
Joaquin Romero <81238879+joakenseg@users.noreply.github.com>
2024-08-07 11:31:22 -0400

    Add files via upload

---
+++ WebDockerTest/WebDockerTest/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using WebDockerTest.Models;
+using WebDockerTest.Services;
+
+namespace WebDockerTest.Controllers
+{
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<List<Users>> Get() =>
+            await _userService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Users>> Get(string id)
+        {
+            var user = await _userService.GetAsync(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        
+        public async Task<ActionResult<Users>> Create(Users newUser)
+        {
+            await _userService.CreateAsync(newUser);
+
+            return CreatedAtAction(nameof(Create), newUser);
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userService.GetAsync(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            await _userService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
